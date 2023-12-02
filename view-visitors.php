862efae8eedf74f500ef016e40245f63fb02d567
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-02 16:06:13 -0600

    Update view-visitors.php

--- view-visitors.php
+++ view-visitors.php
@@ -1,1 +1,31 @@
+<h1>Parks</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+<th>ID</th>
+<th> First Name</th>
+<th>Last Name</th>
+<th>Age</th>
+  </thead>
+    <tbody>
+      
+    </tr>
+ 
 
+<?php
+while ($visitor = $visitors->fetch_assoc())
+{
+?>
+  <tr>
+    <td><?php echo $visitor['visitor_id']; ?></td>
+   <td><?php echo $visitor['visitor_fname']; ?></td>
+  <td><?php echo $visitor['visitor_lname']; ?></td>
+    <td><?php echo $visitor['visitor_age']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
