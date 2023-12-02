146a6760f30e427501c577e563a83a260adc0bf2
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-02 15:38:46 -0600

    Update model-parks.php

--- model-parks.php
+++ model-parks.php
@@ -1,1 +1,20 @@
 
+<?php
+
+
+
+function selectParks() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT park_id, park_name, park_state FROM `park`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
