598c9d2e721b16db97b440ef0994332cf1cb9625
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-02 15:23:37 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,1 +1,12 @@
-
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('159.89.47.44', 'paytonra_finalprojectuser', 'PaytonandLanna', 'paytonra_finialproject');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
