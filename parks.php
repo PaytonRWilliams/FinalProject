6300b7a6158f4ef3e3d55e266fff36927eb2db5b
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-02 15:13:35 -0600

    Update parks.php

--- parks.php
+++ parks.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-parks.php");
 
+$pageTitle = "Parks";
+include "view-header.php";
+$instructors = selectParks();
+include "view-parks.php";
+include "view-footer.php";
+?>
