275c6accb84ddfc2f1aa49e2b9844b1c81c992cb
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-02 15:50:42 -0600

    Update view-parks.php

--- view-parks.php
+++ view-parks.php
@@ -1,1 +1,29 @@
+<h1>Parks</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+<th>ID</th>
+<th>Name</th>
+<th>State</th>
+  </thead>
+    <tbody>
+      
+    </tr>
+ 
 
+<?php
+while ($park = $parks->fetch_assoc())
+{
+?>
+  <tr>
+    <td><?php echo $park['park_id']; ?></td>
+   <td><?php echo $park['park_name']; ?></td>
+    <td><?php echo $park['park_state']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
