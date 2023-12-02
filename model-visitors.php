c163fc593c981e0f58a5203386b95de63db6372c
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-02 16:15:11 -0600

    Update model-visitors.php

--- model-visitors.php
+++ model-visitors.php
@@ -1,1 +1,20 @@
 
+<?php
+
+
+
+function selectVisitors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT visitor_id, visitor_fname, visitor_lname, visitor_age FROM `vistor`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
