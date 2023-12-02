dee111d8e8ba6e07e383ce07ddae888b47a210ad
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-02 15:12:09 -0600

    Update visitors.php

--- visitors.php
+++ visitors.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-visitors.php");
 
+$pageTitle = "Visitors";
+include "view-header.php";
+$courses = selectVisitors();
+include "view-visitors.php";
+include "view-footer.php";
+?>
