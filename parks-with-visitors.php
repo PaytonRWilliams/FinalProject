aa201fe6edc1a82a259ccfff723694caaf22f6d6
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-02 15:24:15 -0600

    Update parks-with-visitors.php

--- parks-with-visitors.php
+++ parks-with-visitors.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-parks-with-visitors.php");
 
+$pageTitle = "Parks with Visitors";
+include "view-header.php";
+$parks = selectParks();
+include "view-parks-with-visitors.php";
+include "view-footer.php";
+?>
