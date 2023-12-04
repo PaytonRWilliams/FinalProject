156a0726b1265e02df4a382b58fac2ddb96d6d0a
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-04 15:57:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 <!-- Example for adding Bootstrap CSS and JS files -->
-<link rel="stylesheet" href="path/to/bootstrap.min.css">
+<link rel="stylesheet" href="path/to/bootstrap.css">
 <script src="path/to/bootstrap.min.js"></script>
 
 <h1>Final Project</h1>
