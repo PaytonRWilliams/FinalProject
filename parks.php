2455a3910e37a389a712cad4dcdb873250f8f14b
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-04 16:10:12 -0600

    Update parks.php

--- parks.php
+++ parks.php
@@ -3,6 +3,7 @@ require_once("util-db.php");
 require_once("model-parks.php");
 
 $pageTitle = "Parks";
+<link rel="stylesheet" href="bootstrap.css">
 include "view-header.php";
 $parks = selectParks();
 include "view-parks.php";
