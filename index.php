69540070082245daceb4296b704bd45d7e0d5f1d
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-04 15:20:00 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 $pageTitle = "Home";
 include "view-header.php";
-include "style.css";
 ?>
+<link rel="stylesheet" type="text/css" href="style.css">
 <h1>Final Project</h1>
 <?php
 include "view-footer.php";
 ?>
-
