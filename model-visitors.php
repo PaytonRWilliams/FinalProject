df865fd95ee3ba551e645c12fdf811a41479eb7e
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-04 16:34:26 -0600

    Update model-visitors.php

--- model-visitors.php
+++ model-visitors.php
@@ -1,8 +1,5 @@
 
 <?php
-
-
-
 function selectVisitors() {
     try {
         $conn = get_db_connection();
@@ -17,4 +14,43 @@ function selectVisitors() {
     }
 }
 
+function insertVisitor($vFname, $vLname, $vAge) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO 'visitor'('visitor_fname', 'visitor_lname', 'visitor_age') VALUES (?,?,?)");
+          $stmt->bind_param("ssi", $vFname, $vLname, $vAge);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateVisitor($vFname, $vLname, $vAge, $vid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update 'visitor' set 'visitor_fname' = ?, 'visitor_lname' = ?, 'visitor_age' = ? where visitor_id = ?);
+          $stmt->bind_param("ssii", $vFname, $vLname, $vAge, $vid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteVisitor($vid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from visitor where visitor_id=?");
+          $stmt->bind_param("i", $vid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
