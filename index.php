36c040723384b59579f32f14f2ae6cc95bc2b137
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-04 16:28:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,47 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<!-- Example for adding Bootstrap CSS and JS files -->
-<link rel="stylesheet" href="bootstrap.css">
 
-<h1>Final Project</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final Project</title>
+    <!-- Example for adding Bootstrap CSS file -->
+    <link rel="stylesheet" href="bootstrap.css">
+    <style>
+        body {
+            /* Set background properties */
+            background-image: url('webnational.jpg');
+            background-size: cover; /* or 'contain' based on your preference */
+            background-position: center center;
+            background-repeat: no-repeat;
+            /* Set other styles as needed */
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            /* Add more styles as needed */
+        }
+
+        /* Add your other CSS styles here */
+
+        h1 {
+            color: #fff; /* Set the color of your heading text */
+        }
+
+        /* Add more styles for other elements as needed */
+
+    </style>
+</head>
+<body>
+
+    <h1>Final Project</h1>
+
+</body>
+</html>
+
 <?php
 include "view-footer.php";
 ?>
+
