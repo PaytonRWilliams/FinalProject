4760d7727c04845c32564bf89af3e4143cdf25a6
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-05 16:16:08 -0600

    Update view-visitors-editform.php

--- view-visitors-editform.php
+++ view-visitors-editform.php
@@ -1,9 +1,9 @@
-
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editVisitorModal<?php echo $visitor['visitor_id']; ?>">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-  </svg>
+<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editVisitorModal<?php echo $visitor['visitor_id']; ?>">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+    </svg>
 </button>
 
 <!-- Modal -->
@@ -18,7 +18,7 @@
         <form method="post" action="">
           <div class="mb-3">
             <label for="vFname<?php echo $visitor['visitor_id']; ?>" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="vFname<?php echo $visitor['visitor_id']; ?>" name="vFname" value="<?php echo $visitor['visitor_fname']; ?>" >
+            <input type="text" class="form-control" id="vFname<?php echo $visitor['visitor_id']; ?>" name="vFname" value="<?php echo $visitor['visitor_fname']; ?>">
           </div>
           <div class="mb-3">
             <label for="vLname<?php echo $visitor['visitor_id']; ?>" class="form-label">Last Name</label>
@@ -26,11 +26,11 @@
           </div>
           <div class="mb-3">
             <label for="vAge<?php echo $visitor['visitor_id']; ?>" class="form-label">Age</label>
-            <input type="int" class="form-control" id="vAge<?php echo $visitor['visitor_id']; ?>" name="vAge" value="<?php echo $visitor['visitor_age']; ?>">
+            <input type="text" class="form-control" id="vAge<?php echo $visitor['visitor_id']; ?>" name="vAge" value="<?php echo $visitor['visitor_age']; ?>">
           </div>
-           <input type="hidden" name="vid" value="<?php echo $visitor['visitor_id']; ?>">
+            <input type="hidden" name="vid" value="<?php echo $visitor['visitor_id']; ?>">
             <input type="hidden" name="actionType" value="edit">
-          <button type="submit" class="btn btn-primary">Save</button>
+          <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
     </div>
