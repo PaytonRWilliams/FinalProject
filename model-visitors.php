f186d836bbde3f5fd3a3022e84c7a427a4b677f0
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-05 17:08:43 -0600

    Update model-visitors.php

--- model-visitors.php
+++ model-visitors.php
@@ -32,7 +32,7 @@ function updateVisitor($vFname, $vLname, $vAge, $vid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("UPDATE `vistor` SET `visitor_id`='[value-1]',`visitor_fname`='[value-2]',`visitor_lname`='[value-3]',`visitor_age`='[value-4]' WHERE 1");
-        $stmt->bind_param("ssii", $vFname, $vLname, $vAge, $vid);
+        $stmt->bind_param("ssii", $vid, $vFname, $vLname, $vAge);
         $success = $stmt->execute();
         $conn->close();
         return $success;
