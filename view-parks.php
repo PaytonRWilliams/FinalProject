70f689d33f8b9ba42e4192ed2d00815bd8bb89bb
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-05 11:31:09 -0600

    Update view-parks.php

--- view-parks.php
+++ view-parks.php
@@ -1,31 +1,25 @@
-<h1>Parks</h1>
-<div class="table-responsive">
-  <link rel="stylesheet" href="bootstrap.css">
-  <table class="table">
-  <thead>
-    <link rel="stylesheet" href="bootstrap.css">
-    <tr>
-<th>ID</th>
-<th>Name</th>
-<th>State</th>
-  </thead>
-    <tbody>
-      
-    </tr>
- 
-
-<?php
-while ($park = $parks->fetch_assoc())
-{
-?>
-  <tr>
-    <td><?php echo $park['park_id']; ?></td>
-   <td><?php echo $park['park_name']; ?></td>
-    <td><?php echo $park['park_state']; ?></td>
-  </tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
+<h1 class="mt-4">Parks</h1>
+<div class="card">
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>State</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while ($park = $parks->fetch_assoc()) { ?>
+            <tr>
+              <td><?php echo $park['park_id']; ?></td>
+              <td><?php echo $park['park_name']; ?></td>
+              <td><?php echo $park['park_state']; ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
