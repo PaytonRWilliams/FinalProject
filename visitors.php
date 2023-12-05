afc995698576506fbc7a0d3adea922935641bd79
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-05 08:20:26 -0600

    Update visitors.php

--- visitors.php
+++ visitors.php
@@ -4,6 +4,17 @@ require_once("model-visitors.php");
 
 $pageTitle = "Visitors";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertCourse($_POST['vFname'], $_POST['vLname'], $_POST['vAge'])) {
+        echo '<div class="alert alert-success" role="alert">Visitor added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    
 $visitors = selectVisitors();
 include "view-visitors.php";
 include "view-footer.php";
