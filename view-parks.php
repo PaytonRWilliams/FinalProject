6c0f8161c55afa55e6edb04013951e9c0ce31fe6
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-05 11:35:02 -0600

    Update view-parks.php

--- view-parks.php
+++ view-parks.php
@@ -1,25 +1,15 @@
-<h1 class="mt-4">Parks</h1>
-<div class="card">
-  <div class="card-body">
-    <div class="table-responsive">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>State</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php while ($park = $parks->fetch_assoc()) { ?>
-            <tr>
-              <td><?php echo $park['park_id']; ?></td>
-              <td><?php echo $park['park_name']; ?></td>
-              <td><?php echo $park['park_state']; ?></td>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
+
+
+<h1>Parks</h1>
+
+<div class="card-group">
+  <?php while ($park = $parks->fetch_assoc()): ?>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">ID: <?php echo $park['park_id']; ?></h5>
+        <p class="card-text">Name: <?php echo $park['park_name']; ?></p>
+        <p class="card-text">State: <?php echo $park['park_state']; ?></p>
+      </div>
     </div>
-  </div>
+  <?php endwhile; ?>
 </div>
