856812ae8e4e5045da3f8fcfd45fabf60f6d2ad1
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-05 17:14:06 -0600

    Update visitors.php

--- visitors.php
+++ visitors.php
@@ -15,7 +15,7 @@ if (isset($_POST['actionType'])) {
       }
       break;
     case "edit":
-      if (updateVisitor($_POST['vFname'], $_POST['vLname'], $_POST['vAge'], $_POST['vid'])) {
+      if (updateVisitor( $_POST['vid'], $_POST['vFname'], $_POST['vLname'], $_POST['vAge'])) {
         echo '<div class="alert alert-success" role="alert">Visitor edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
