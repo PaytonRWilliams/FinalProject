6fe7647ba8b9f2a1317b24a7f2c68a93dac4e782
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-05 19:57:15 -0600

    Update view-parks.php

--- view-parks.php
+++ view-parks.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-     <?php include "view-parks-editform.php"; ?>
+    
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
@@ -57,6 +57,27 @@
           </div>
         </div>
       <?php endwhile; ?>
+
+
+ <?php include "view-parks-editform.php"; ?>
+        <form method="post" action="">
+          <input type="hidden" name="Pid" value="<?php echo $park['park_id']; ?>">
+          <input type="hidden" name="actionType" value="Delete">
+          <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-trash" viewBox="0 0 16 16">
+              <path
+                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6ZM14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+            </svg>
+          </button>
+        </form>
+
+
+
+
+
+
+         
     </div>
   </div>
 
