21e54616a890279cf98369c4d101cd94318c7d54
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-12-05 18:18:51 -0600

    Update model-parks.php

--- model-parks.php
+++ model-parks.php
@@ -17,4 +17,55 @@ function selectParks() {
     }
 }
 
+
+
+
+
+function insertParks($Pname, $Pstate) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `park` (`park_name`, `park_state`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $Pname, $Pstate);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateVisitor($Pname, $Pstate, $Pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `park` SET `park_name`=?, `park_state`=? WHERE `park_id`=?");
+        $stmt->bind_param("ssi", $Pname, $Pstate, $Pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteVisitor($Pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `park` WHERE `park`.`park_id` = ?");
+        $stmt->bind_param("i", $Pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+
+
+
+
 ?>
