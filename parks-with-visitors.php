238f2d8a09789baf12c572eb0e41c8a7d79537a1
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-05 18:46:38 -0600

    Update parks-with-visitors.php

--- parks-with-visitors.php
+++ parks-with-visitors.php
@@ -4,7 +4,34 @@ require_once("model-parks-with-visitors.php");
 
 $pageTitle = "Parks with Visitors";
 include "view-header.php";
-$parks = selectParks();
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertVisit($_POST['pid'], $_POST['vid'], $_POST['visit_date'])) {
+        echo '<div class="alert alert-success" role="alert">Visit added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateVisit($_POST['pid'], $_POST['vid'], $_POST['visit_date'], $_POST['tid'])) {
+        echo '<div class="alert alert-success" role="alert">Visit edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteVisit($_POST['tid'])) {
+        echo '<div class="alert alert-success" role="alert">Visit deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+$visits = selectVisits();
 include "view-parks-with-visitors.php";
 include "view-footer.php";
 ?>
