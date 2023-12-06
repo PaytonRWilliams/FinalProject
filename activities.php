7e0224cfb728d0cb1df9f5f17341f854634aa2d6
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-05 18:08:17 -0600

    Update activities.php

--- activities.php
+++ activities.php
@@ -1,1 +1,37 @@
+<?php
+require_once("util-db.php");
+require_once("model-activities.php");
 
+$pageTitle = "Activities";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertActivity($_POST['aName'], $_POST['aType'], $_POST['aCost'])) {
+        echo '<div class="alert alert-success" role="alert">Activity added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateActivity( $_POST['aName'], $_POST['aType'], $_POST['aCost'], $_POST['aid'])) {
+        echo '<div class="alert alert-success" role="alert">Activity edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+    break;
+    case "Delete":
+      if (deleteActivity($_POST['aid'])) {
+        echo '<div class="alert alert-success" role="alert">Activity deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+$visitors = selectActivities();
+include "view-activities.php";
+include "view-footer.php";
+?>
