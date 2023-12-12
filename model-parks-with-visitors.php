c3718a3fd79a6ef7847b991125543f4ae6ae5d9b
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-12 09:54:38 -0600

    Update model-parks-with-visitors.php

--- model-parks-with-visitors.php
+++ model-parks-with-visitors.php
@@ -1,1 +1,100 @@
+<?php
+function selectInstructors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT instructor_id, instructor_name, office_number FROM `instructor`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
+function selectCoursesByInstructor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time, section_id, s.instructor_id FROM `course` c join section s on s.course_id = c.course_id where s.instructor_id=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectInstructorsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT instructor_id, instructor_name FROM `instructor` order by instructor_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectCoursesForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT course_id, course_number FROM `course` order by course_number");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertSection($iid, $cid, $sem, $room, $time) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `section` (`instructor_id`, `course_id`, `semester`, `room`, `day_time`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("iisss", $iid, $cid, $sem, $room, $time);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSection($iid, $cid, $sem, $room, $time, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `section` set `instructor_id` = ?, `course_id` = ?, `semester` = ?, `room` = ?, `day_time` = ? where section_id = ?");
+        $stmt->bind_param("iisssi", $iid, $cid, $sem, $room, $time, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSection($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from section where section_id=?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
