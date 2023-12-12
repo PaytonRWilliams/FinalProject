4ea03cfc74b58a9ec5ae77b9fcdbd5be29f76654
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-12 14:57:11 -0600

    Update model-visitors.php

--- model-visitors.php
+++ model-visitors.php
@@ -3,7 +3,7 @@
 function selectVisitors() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT visitor_id, visitor_fname, visitor_lname, visitor_age FROM `vistor`");
+        $stmt = $conn->prepare("SELECT visitor_id, visitor_fname, visitor_lname, visitor_age FROM `visitor`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
