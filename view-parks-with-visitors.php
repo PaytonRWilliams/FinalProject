f23ce9e7132c72d1cec1d26214cb16ee54866558
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-12-13 15:33:53 -0600

    Update view-parks-with-visitors.php

--- view-parks-with-visitors.php
+++ view-parks-with-visitors.php
@@ -33,7 +33,7 @@ include "view-parks-with-visitors-editform.php";
           </div>
       <div class="col-auto">
         <form method="post" action="">
-          <input type="hidden" name="pid" value="<?php echo $visitor['visit_id']; ?>">
+          <input type="hidden" name="tid" value="<?php echo $visitor['visit_id']; ?>">
           <input type="hidden" name="actionType" value="Delete">
           <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
